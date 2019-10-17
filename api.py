7c01ff5fde40afa7ce4d23724b5e3bb853edef8d
Maryam Biazaran <maryam.biazaran@gmail.com>
2019-10-17 17:09:40 -0500

    fancy stuff

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 from flask import Flask,request
-from flask_restful import Resource, Api, reqparse
-from requests import put, get
+from flask_restful import Resource, Api, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,25 +8,57 @@ api = Api(app)
 # parser.add_argument('rate', type=int, help='Rate to charge for this resource')
 # args = parser.parse_args()
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-api.add_resource(HelloWorld,'/','/sayhi')
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '????'},
+    'todo3': {'task': 'profit!s'},
+}
 
-todos = {}
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404,message="TODO {} doesn't exist".format(todo_id))
 
-class TodoSimple(Resource):
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
     def get(self,todo_id):
-        return {todo_id: todos[todo_id]},201, {'Etag': 'some-opaque-string'}
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self,todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '',204
 
     def put(self,todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
 
 
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo'))+1
+        todo_id = 'todo%i'%todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id],201
+
+# Actually setup the API
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo,'/todos/<todo_id>')
 
 
 if __name__ == '__main__':
