58943e42b10f5b9f5d191b47c659cabbd1c4bace
Maryam Biazaran <maryam.biazaran@gmail.com>
2019-10-21 16:03:04 -0500

    finished PUT and DELETE for individual TODOs, need to work on full list and POST new todo

--- api.py
+++ api.py
@@ -3,13 +3,12 @@ from models import Todo, Tag
 # from flask import request
 from flask_restful import Resource, Api, reqparse, abort, fields, marshal_with
 import json
+from helpers import str2bool
 
 #app = Flask(__name__)
 api = Api(app)
 
-# parser = reqparse.RequestParser()
-# parser.add_argument('rate', type=int, help='Rate to charge for this resource')
-# args = parser.parse_args()
+# =======================================
 
 tag_fields = {
     'id' : fields.Integer,
@@ -23,6 +22,7 @@ todo_fields = {
     'category': fields.String(attribute='tag.name'),
 }
 
+# =======================================
 
 def get_incomplete_todos(tag = None):
     if tag is None:
@@ -33,12 +33,7 @@ def get_incomplete_todos(tag = None):
 def get_inactive_posts():
     return Todo.query.filter_by(completed=False).order_by(-Todo.id).all()
 
-
-# TODOS = {
-#     'todo1': {'task': 'build an API'},
-#     'todo2': {'task': '????'},
-#     'todo3': {'task': 'profit!s'},
-# }
+# =======================================
 
 def abort_if_todo_doesnt_exist(todo_id):
     try:
@@ -49,40 +44,73 @@ def abort_if_todo_doesnt_exist(todo_id):
             abort(404,message="TODO {} doesn't exist".format(todo_id))
     except TypeError:
         abort(404,message="TODO {} doesn't exist".format(todo_id))
-    
-    
+
+def find_or_create_new_tag(tag_name):
+
+    the_tag = Tag.query.filter_by(name=tag_name).first()
+    if the_tag:
+        return the_tag
+    else:
+        new_tag = Tag(tag_name)
+        db.session.add(new_tag)
+        db.session.commit()
+        return new_tag
+
+# =======================================
 
 parser = reqparse.RequestParser()
 parser.add_argument('task')
+parser.add_argument('category')
+parser.add_argument('completed', type=str2bool, default=None, help="Is the task complete?")
 
+# =======================================
 
 # Todo
 # shows a single todo item and lets you delete a todo item
+@api.resource('/todos/<todo_id>')
 class TodosController(Resource):
+    
+    #GET
     @marshal_with(todo_fields, envelope='Todo')
     def get(self,todo_id):
         abort_if_todo_doesnt_exist(todo_id)
         the_todo = Todo.query.filter_by(id=int(todo_id)).first()
         return the_todo,200
+
+    #PUT
+    @marshal_with(todo_fields, envelope='Todo')    
+    def put(self,todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        the_todo = Todo.query.filter_by(id=int(todo_id)).first()
+        args = parser.parse_args()
+        if args['task']:
+            the_todo.task = args['task']
+        if args['completed'] != None:
+            print('=============\n',args['completed'])
+            the_todo.completed = bool(args['completed'])
+        if args['category']:
+            the_todo.tag = find_or_create_new_tag(args['category'])
+
+        db.session.add(the_todo)
+        db.session.commit()
+        return the_todo, 201
+
+    #DELETE
+    def delete(self,todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        the_todo = Todo.query.filter_by(id=int(todo_id)).first()
+        db.session.delete(the_todo)
+        db.session.commit()
+        return '', 204
         
-        
-
-    # def delete(self,todo_id):
-    #     abort_if_todo_doesnt_exist(todo_id)
-    #     del TODOS[todo_id]
-    #     return '',204
-
-    # def put(self,todo_id):
-    #     args = parser.parse_args()
-    #     task = {'task': args['task']}
-    #     TODOS[todo_id] = task
-    #     return task, 201
-
+# =======================================
 
 # TodoList
 # shows a list of all todos, and lets you POST to add new tasks
 
 # class TodoList(Resource):
+#     @marshal_with(todo_fields, envelope='Todo')
+#     # STILL NEED TO WORK ON THIS
 #     def get(self):
 #         return TODOS
 
@@ -95,7 +123,7 @@ class TodosController(Resource):
 
 # Actually setup the API
 # api.add_resource(TodoList, '/todos')
-api.add_resource(TodosController,'/todos/<todo_id>')
+# api.add_resource(TodosController,'/todos/<todo_id>')
 
 
 if __name__ == '__main__':
